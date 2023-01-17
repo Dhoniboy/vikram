6b26293ed07a512679f45139b5b95870f770929b
Dhoniboy <103166082+Dhoniboy@users.noreply.github.com>
2023-01-17 10:29:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,15 +43,14 @@ class Bot(Client):
         temp.U_NAME = me.username
         temp.B_NAME = me.first_name
         self.username = '@' + me.username
+        #web-response
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
-        logging.info(script.LOGO)
-        tz = pytz.timezone('Asia/Kolkata')
-        today = date.today()
-        now = datetime.now(tz)
-        time = now.strftime("%H:%M:%S %p")
-        await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
-
+        
     async def stop(self, *args):
         await super().stop()
         logging.info("Bot stopped. Bye.")
