511f569319d31a96f10a1c4569b708276e653eaa
Dhoniboy <103166082+Dhoniboy@users.noreply.github.com>
2023-01-17 08:19:33 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -103,11 +103,16 @@ Cᴏᴍᴍᴀɴᴅs Aɴᴅ Usᴀɢᴇ:
 • /delallg - <code>Tᴏ ᴅᴇʟᴇᴛᴇ ᴀʟʟ Gғɪʟᴛᴇʀs ғʀᴏᴍ ᴛʜᴇ ʙᴏᴛ's ᴅᴀᴛᴀʙᴀsᴇ.</code>
 • /deletefiles - <code>Tᴏ ᴅᴇʟᴇᴛᴇ CᴀᴍRɪᴘ ᴀɴᴅ PʀᴇDVD Fɪʟᴇs ғʀᴏᴍ ᴛʜᴇ ʙᴏᴛ's ᴅᴀᴛᴀʙᴀsᴇ.</code>"""
 
-    STATUS_TXT = """<b>★ Tᴏᴛᴀʟ Fɪʟᴇs: <code>{}</code>
-★ Tᴏᴛᴀʟ Usᴇʀs: <code>{}</code>
-★ Tᴏᴛᴀʟ Cʜᴀᴛs: <code>{}</code>
-★ Usᴇᴅ Sᴛᴏʀᴀɢᴇ: <code>{}</code>
-★ Fʀᴇᴇ Sᴛᴏʀᴀɢᴇ: <code>{}</code></b>"""
+    STATUS_TXT = """<b>✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵
+╔════❰ ꜱᴛᴀᴛᴜꜱ ❱═❍⊱❁۪۪
+║┏━━━━━━━━━━━━━━━➣
+║┣⪼  Tᴏᴛᴀʟ Fɪʟᴇs:<code>{}</code>
+║┣⪼  Tᴏᴛᴀʟ Usᴇʀs: <code>{}</code>
+║┣⪼  Tᴏᴛᴀʟ Cʜᴀᴛs: <code>{}</code>
+║┣⪼  Usᴇᴅ Sᴛᴏʀᴀɢᴇ: <code>{}</code>
+║┣⪼  Fʀᴇᴇ Sᴛᴏʀᴀɢᴇ: <code>{}</code>
+║┗━━━━━━━━━━━━━━━➣
+╚══════════════════❍⊱❁</b>"""
 
     LOG_TEXT_G = """#NewGroup
 Gʀᴏᴜᴘ = {}(<code>{}</code>)
